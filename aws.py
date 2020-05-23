100a881608c6340fb8b4fe0649514253609ccdd8
Dmytro_ubuntu <dmutro25@yahoo.com>
2020-05-23 14:52:51 -0400

    asd#

--- aws.py
+++ aws.py
@@ -1,5 +1,11 @@
 import boto3
+
 session = boto3.Session(profile_name='tudi')
 ec2 = session.resource('ec2')
-for i in ec2.instances.all():
-    print(i)
+
+def list_instanses():
+    for i in ec2.instances.all():
+        print(i)
+
+if __name__ == '__main__':
+    list_instanses()
