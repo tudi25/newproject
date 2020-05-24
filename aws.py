7501cc7646036ae0bb5ce8d795dca6b1d76375c4
Dmytro_ubuntu <dmutro25@yahoo.com>
2020-05-23 20:27:12 -0400

    new

--- aws.py
+++ aws.py
@@ -5,6 +5,67 @@ session = boto3.Session(profile_name='tudi')
 ec2 = session.resource('ec2')
 
 @click.group()
+def cli():
+        "Manage snapshots"
+
+@cli.group('snapshots')
+def snapshots():
+        "Command for snapshots"
+
+@snapshots.command('list')
+@click.option('--project', default=None, help="Only snapshots for project(tag instanse:<name>)")
+def list_volumes(project):
+    "List of Snapshots"
+
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Instanse', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(', '.join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+    return
+@cli.group('volumes')
+def volumes():
+        "Command for volumes"
+
+@volumes.command('list')
+@click.option('--project', default=None, help="Only instances for project(tag instanse:<name>)")
+def list_volumes(project):
+    "List of Volumes"
+
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Instanse', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(', '.join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+    return
+
+@cli.group('instances')
 def instances():
         """Command for instances"""
 
@@ -47,4 +108,4 @@ def stop_instanses(project):
     return
 
 if __name__ == '__main__':
-    instances()
+    cli()
