b192071e83e6c6b2444f49d118b04a5df17803bd
tudi25 <65698702+tudi25@users.noreply.github.com>
2020-06-15 15:11:52 -0400

    Delete 12.py

--- 12.py
+++
@@ -1,9 +0,0 @@
-print("finaly")
-for i in range(10):
-    print(str(i ) + " Heloo")
-k = list(range(20))
-print(k[0:5])
-<<<<<<< HEAD
-=======
-print("qw")
->>>>>>> 51eb2aacc83684d4b912b1e14bcf442264601507
